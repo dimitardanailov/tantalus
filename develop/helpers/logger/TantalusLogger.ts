5dd87634ecf81cdad2ac238073b78d74b44d830f
Dimitar Danailov <dimityr.danailov@gmail.com>
2018-08-20 19:21:13 +0300

    Application has a custom Logger.

---
+++ develop/helpers/logger/TantalusLogger.ts
@@ -0,0 +1,9 @@
+const appName = require('./../package').name;
+const log4js = require('log4js');
+const logger = log4js.getLogger(appName);
+
+export class TantalusLogger {
+	static info(message) {
+		logger.info(message);
+	}
+}
