eb8b5925cfb361e1c425d5f2aa4b143f37e6dcb2
Dimitar Danailov <dimityr.danailov@gmail.com>
2018-08-25 16:23:31 +0300

    This commit "unused" source code.

--- develop/helpers/aws/TantalusAWSS3.ts
+++ develop/helpers/aws/TantalusAWSS3.ts
@@ -1,9 +1,6 @@
 import AWS = require('aws-sdk');
-import fs = require('fs');
-import zlib = require('zlib');
 import { TantalusLogger } from "../logger/TantalusLogger";
-import { TantalusZIPStream } from './TantalusZIPStream';
-import { Writable, Readable, Stream } from 'stream';
+import { Stream } from 'stream';
 
 export class TantalusAWSS3 {
 
@@ -31,47 +28,6 @@ export class TantalusAWSS3 {
 		});
 
 		return pass;
-
-		/*
-		s3.upload({
-			Bucket: TantalusAWSS3.getBucketName(),
-			Key : s3Key,
-			Body: pass
-		}, (err, data) => {
-			TantalusLogger.debugVariable(data);
-		});
-
-		return pass;
-		*/
-
-		/*
-		this.s3.putObject().promise().then(data => {
-			TantalusLogger.info('Success');
-		});*/
-
-		/*
-		this.s3.upload({ Body: body }).
-			on('httpUploadProgress', function(evt) {
-				console.log('Progress:', evt.loaded, '/', evt.total); 
-			}).
-			send(function(err, data) { console.log(err, data) });*/
-
-		// archiver.output = fs.createWriteStream(join(__dirname, s3FileName));
-		// console.log(archiver.output);
-		//archiver.output = fs.createWriteStream(join(__dirname, s3FileName));
-
-		/*
-		
-
-		TantalusLogger.debugVariable(params.Key);
-		s3.putObject(params).promise();
-		
-		var putObjectPromise = s3.putObject(params).promise();
-		putObjectPromise.then((data) => {
-			TantalusLogger.info('Success');
-		}).catch(function(error) {
-			TantalusLogger.debugVariable(error);
-		}); */
 	}
 
 	private static createAWSCongiguration() {
