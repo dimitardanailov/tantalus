6e5d57e53c40e5d5ec00e9d5830f5fa573275381
Dimitar Danailov <dimityr.danailov@gmail.com>
2018-10-19 13:58:08 +0300

    Fix path of worker file.

--- develop/index.ts
+++ develop/index.ts
@@ -1,5 +1,5 @@
 if (process.env.SERVICE_NAME === 'api') {
 	require('./api/server');
 } else if (process.env.SERVICE_NAME === 'worker') {
-	require('./agenda/server');
+	require('./agenda/worker');
 }
